5b5656d02d449e6af5bded437cd946dd7799277d
arunabharjun <sunnyarj114@gmail.com>
2020-05-08 00:00:20 +0530

    initial commit

---
+++ MERN_BACKEND_BOILERPLATE/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+require('dotenv').config();
+
+// app
+const app = express();
+
+//envitonment
+const port = process.env.PORT || 8000;
+const env = process.env.ENV || 'DEVELOPMENT';
+const client_app = process.env.CLIENT_URL;
+
+// middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+// cors
+app.use(cors());
+
+// routes
+app.get('/api', (req, res) => {
+	res.json({
+		time: Date().toString()
+	});
+});
+
+// app listen
+app.use(function(req, res) {
+	res.status(404).json({ error: '404 : Page not found' });
+});
+
+app.listen(port, () => {
+	console.log(
+		`[API] : Server running on port ${port} | ENVIRONMENT : ${env} \n[API] : CLIENT APP : ${client_app}`
+	);
+});
---
+++ README.md
@@ -0,0 +1,2 @@
+# boilerplates
+A repo to quickly clone boilerplate code to kick start a project
